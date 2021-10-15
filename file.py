9495b7c1e76d5b8bb2b6ce11b59586546b9d5e7e
siudro <83130573+siudro@users.noreply.github.com>
2021-10-15 23:38:05 +0300

    add a useless file

---
+++ file.py
@@ -0,0 +1,7 @@
+num1=int(input("enter number 1: "))
+num2=int(input("enter number 1: "))
+result=num1+num2
+print("_______________________")
+print("result is ",result)
+print("_______________________")
+input("press enter to exit...")
